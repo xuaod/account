4129f9c9d5b7b5074cf647cd54b6ed6be0f42737
xuaod <m44594033@gmail.com>
2025-08-21 05:36:33 -0300

    Update teste.bat

--- teste.bat
+++ teste.bat
@@ -1,2 +1,24 @@
-echo Hello World
+@echo off
+title Hello World Batch Script
+color 0A
+echo.
+echo    ===============================
+echo        HELLO WORLD SCRIPT
+echo    ===============================
+echo.
+echo    Este arquivo BAT demonstra:
+echo    - Como exibir uma mensagem
+echo    - Como personalizar a aparência
+echo    - Como evitar fechamento automatico
+echo.
+echo    Hello World!
+echo.
+echo    Script executado em: %date% %time%
+echo.
 pause
+cls
+echo.
+echo    Obrigado por usar este script!
+echo    Desenvolvido especialmente para você
+echo.
+timeout /t 3 /nobreak >nul
